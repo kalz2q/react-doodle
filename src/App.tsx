5b2fa139544b74209c1113dc9f5613de5da683cb
kalz2q <katsu95i@gmail.com>
2022-10-26 19:30:24 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const { useEffect, useState } = React;
 
 function App() {
+  const [png, setPng] = useState<string | null>(null);
+
+  useEffect(() => {
+    const canvasElem = document.createElement("canvas");
+    const context = canvasElem.getContext("2d");
+    canvasElem.width = 400;
+    canvasElem.height = 200;
+
+    // draw
+    if (context !== null) {
+      context.lineWidth = 10;
+      context.strokeStyle =  "blue"
+      context.strokeRect(0,0,canvasElem.width, canvasElem.height);
+      // context.fillRect(0, 0, canvasElem.width, canvasElem.height);
+      context.fillStyle = "yellow";
+      context.fillRect(50, 50, 50, 50);
+      context.fillRect(canvasElem.width - 100, 50, 50, 50);
+    }
+    setPng(canvasElem.toDataURL());
+  }, [png]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>お絵かきアプリ</h1>
+      {png && <img alt="" src={png} />}
     </div>
   );
 }
