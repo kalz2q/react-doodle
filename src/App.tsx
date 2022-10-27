e53090714e7a96846ba038e230f6f60929bd220e
kalz2q <katsu95i@gmail.com>
2022-10-28 00:45:29 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -12,63 +12,73 @@ function App() {
     y: number;
   };
 
-  const startPaint = useCallback((event: MouseEvent) => {
-    const coordinates = getCoordinates(event);
-    if (coordinates) {
-      setMousePosition(coordinates);
-      setIsPainting(true);
-    }
-  }, []);
-
-  const paint = useCallback(
-    (event: MouseEvent) => {
-      if (isPainting) {
-        const newMousePosition = getCoordinates(event);
-        if (mousePosition && newMousePosition) {
-          drawLine(mousePosition, newMousePosition);
-          setMousePosition(newMousePosition);
-        }
-      }
-    },
-    [isPainting, mousePosition]
-  );
 
 
-  const getCoordinates = (event: MouseEvent) => {
-    if (event.type === 'mousedown') {
-      startX = event.pageX - canvasElem.offsetLeft - borderWidth;
-      startY = event.pageY - canvasElem.offsetTop - borderWidth;
-    } else if (event.type === 'mousemove') {
-      x = event.pageX - canvasElem.offsetLeft - borderWidth;
-      y = event.pageY - canvasElem.offsetTop - borderWidth;
-      context.beginPath();
-      context.moveTo(startX, startY);
-      context.lineTo(x, y);
-      context.stroke();
-      startX = x;
-      startY = y;
-    }
-  }
 
   useEffect(() => {
     const canvasElem = document.createElement("canvas");
     const context = canvasElem.getContext("2d");
+    canvasElem.addEventListener("mousedown", event => startPaint(event));
 
     canvasElem.width = 400;
     canvasElem.height = 200;
 
+
+
+    // ---
     if (context !== null) {
       context.lineWidth = 10;
       context.strokeStyle = "blue"
       context.strokeRect(0, 0, canvasElem.width, canvasElem.height);
 
-      canvasElem.addEventListener("mousedown", event => startPaint(event));
 
       canvasElem.addEventListener("mousemove", event => getCoordinate(event));
 
+
+      const startPaint = useCallback((event: MouseEvent) => {
+        const coordinates = getCoordinates(event);
+        if (coordinates) {
+          setMousePosition(coordinates);
+          setIsPainting(true);
+        }
+      }, []);
+
+      const paint = useCallback(
+        (event: MouseEvent) => {
+          if (isPainting) {
+            const newMousePosition = getCoordinates(event);
+            if (mousePosition && newMousePosition) {
+              drawLine(mousePosition, newMousePosition);
+              setMousePosition(newMousePosition);
+            }
+          }
+        },
+        [getCoordinates]
+      );
+
+
+      const getCoordinates = (event: MouseEvent) => {
+        if (event.type === 'mousedown') {
+          startX = event.pageX - canvasElem.offsetLeft - borderWidth;
+          startY = event.pageY - canvasElem.offsetTop - borderWidth;
+        } else if (event.type === 'mousemove') {
+          x = event.pageX - canvasElem.offsetLeft - borderWidth;
+          y = event.pageY - canvasElem.offsetTop - borderWidth;
+          context.beginPath();
+          context.moveTo(startX, startY);
+          context.lineTo(x, y);
+          context.stroke();
+          startX = x;
+          startY = y;
+        }
+      }
+
+
+
+
     }
     setPng(canvasElem.toDataURL());
-  }, [png]);
+  }, [png, isPainting, mousePosition]);
 
   return (
     <div className="App">
