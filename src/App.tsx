73ed7d43e5d27a4e505cbfc08a7a09b0375339db
kalz2q <katsu95i@gmail.com>
2022-10-27 16:35:54 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,58 @@
 import React from 'react';
 import './App.css';
-const { useEffect, useState } = React;
+const { useEffect, useState, useCallback } = React;
 
 function App() {
   const [png, setPng] = useState<string | null>(null);
 
+  type Coordinate = {
+    x: number;
+    y: number;
+  };
+
+  const [isPainting, setIsPainting] =  useState(false);
+  const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(undefined);
+
+
+  const startPaint = useCallback((event: MouseEvent) => {
+    const coordinates = getCoordinates(event);
+    if (coordinates) {
+        setMousePosition(coordinates);
+        setIsPainting(true);
+    }
+}, []);
+
+
+const paint = useCallback(
+  (event: MouseEvent) => {
+      if (isPainting) {
+          const newMousePosition = getCoordinates(event);
+          if (mousePosition && newMousePosition) {
+              drawLine(mousePosition, newMousePosition);
+              setMousePosition(newMousePosition);
+          }
+      }
+  },
+  [isPainting, mousePosition]
+);
 
 
   useEffect(() => {
     const canvasElem = document.createElement("canvas");
     const context = canvasElem.getContext("2d");
+
     canvasElem.width = 400;
     canvasElem.height = 200;
-    let startX: number, startY: number, x: number, y: number,
-      borderWidth = 10;
-
-
-
-
-    // draw
+    
     if (context !== null) {
       context.lineWidth = 10;
       context.strokeStyle = "blue"
       context.strokeRect(0, 0, canvasElem.width, canvasElem.height);
-      canvasElem.addEventListener("mousedown", event => getCoordinate(event));
+
+      canvasElem.addEventListener("mousedown", event => startPaint(event));
+
       canvasElem.addEventListener("mousemove", event => getCoordinate(event));
+
       const getCoordinate = (event: MouseEvent) => {
         if (event.type === 'mousedown') {
           startX = event.pageX - canvasElem.offsetLeft - borderWidth;
