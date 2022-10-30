f9abf2e8c27a4d2cff08aaa4d2bd72d4fdeebcaf
kalz2q <katsu95i@gmail.com>
2022-10-31 03:34:32 +0900

    kk

--- src/App.tsx
+++ src/App.tsx
@@ -13,86 +13,77 @@ const { useRef } = React;
 // }
 
 function App() {
-  const width = 400;
-  const height = 200;
+  // const width = 400;
+  // const height = 200;
   let canvasRef = useRef<HTMLCanvasElement | null>(null);
-  let mouseX: number | null = null;
-  let mouseY: number | null = null;
+  // if (!canvasRef) return;
+  const context = canvasRef.current;
+  // let mouseX: number | null = null;
+  // let mouseY: number | null = null;
 
-  const getContext = (): CanvasRenderingContext2D => {
-    const canvas: any = canvasRef.current;
-    return canvas.getContext('2d');
-  };
+  // const getContext = (): CanvasRenderingContext2D => {
+  //   const canvas: any = canvasRef.current;
+  //   return canvas.getContext('2d');
+  // };
 
-  const OnClick = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    if (e.button !== 0) { return; }
-    const canvas: any = canvasRef.current;
-    // const rect: IRect = canvas.getBoundingClientRect();
-    const rect = canvas.getBoundingClientRect();
-    const x = ~~(e.clientX - rect.left);
-    const y = ~~(e.clientY - rect.top);
-    Draw(x, y);
-  }
+  // const OnClick = (e: React.MouseEvent<HTMLCanvasElement>) => {
+  //   if (e.button !== 0) { return; }
+  //   const canvas: any = canvasRef.current;
+  //   // const rect: IRect = canvas.getBoundingClientRect();
+  //   const rect = canvas.getBoundingClientRect();
+  //   const x = ~~(e.clientX - rect.left);
+  //   const y = ~~(e.clientY - rect.top);
+  //   Draw(x, y);
+  // }
 
-  const OnMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    if (e.buttons !== 1) { return; }
-    const canvas: any = canvasRef.current;
-    // const rect: IRect = canvas.getBoundingClientRect();
-    const rect = canvas.getBoundingClientRect();
-    const x = ~~(e.clientX - rect.left);
-    const y = ~~(e.clientY - rect.top);
-    Draw(x, y);
-  }
+  // const OnMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
+  //   if (e.buttons !== 1) { return; }
+  //   const canvas: any = canvasRef.current;
+  //   // const rect: IRect = canvas.getBoundingClientRect();
+  //   const rect = canvas.getBoundingClientRect();
+  //   const x = ~~(e.clientX - rect.left);
+  //   const y = ~~(e.clientY - rect.top);
+  //   Draw(x, y);
+  // }
 
-  const DrawEnd = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    mouseX = null;
-    mouseY = null;
-  }
+  // const DrawEnd = (e: React.MouseEvent<HTMLCanvasElement>) => {
+  //   mouseX = null;
+  //   mouseY = null;
+  // }
 
-  const Draw = (x: number, y: number) => {
-    const ctx = getContext();
-    ctx.beginPath();
-    ctx.globalAlpha = 1.0;
-    if (mouseX === null || mouseY === null) {
-      ctx.moveTo(x, y);
-    } else {
-      ctx.moveTo(mouseX, mouseY);
-    }
-    ctx.lineTo(x, y);
-    ctx.lineCap = "round";
-    ctx.lineWidth = 20;
-    // ctx.strokeStyle = "#000000";
-    ctx.strokeStyle = "DarkGreen";
-    ctx.stroke();
-    mouseX = x;
-    mouseY = y;
-  }
+  // const Draw = (x: number, y: number) => {
+  //   const ctx = getContext();
+  //   ctx.beginPath();
+  //   ctx.globalAlpha = 1.0;
+  //   if (mouseX === null || mouseY === null) {
+  //     ctx.moveTo(x, y);
+  //   } else {
+  //     ctx.moveTo(mouseX, mouseY);
+  //   }
+  //   ctx.lineTo(x, y);
+  //   ctx.lineCap = "round";
+  //   ctx.lineWidth = 20;
+  //   // ctx.strokeStyle = "#000000";
+  //   ctx.strokeStyle = "DarkGreen";
+  //   ctx.stroke();
+  //   mouseX = x;
+  //   mouseY = y;
+  // }
 
-  const reset = () => {
-    const ctx = getContext();
-    const canvas: any = canvasRef.current;
-    // ctx.clearRect(0, 0, width, height);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
+  // const reset = () => {
+  //   const ctx = getContext();
+  //   const canvas: any = canvasRef.current;
+  //   // ctx.clearRect(0, 0, width, height);
+  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // }
 
   return (
     <div className="App">
-      <h3>お絵かきアプリ</h3>
       <div>
-        <canvas onMouseDown={OnClick}
-          onMouseMove={OnMove}
-          onMouseUp={DrawEnd}
-          onMouseOut={DrawEnd}
-          ref={canvasRef}
-          width={`${width}px`}
-          height={`${height}px`}
-
-        />
+        <canvas width="400" height="200" className="mycanvas">
+          canvas
+        </canvas>
       </div>
-      <div>
-        <button className="reset" onClick={reset}>リセット</button>
-      </div>
-
     </div>
   );
 }
